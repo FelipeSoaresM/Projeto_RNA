b6bf7e3b5b88060071acd40c032b4a9aa88f8671
Zelmar <zelmar.junior@grendene.com.br>
2024-04-05 23:56:12 -0300

    atualiza

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import MyForm from "./components/widget-form";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <MyForm />
   </React.StrictMode>
